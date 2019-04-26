b7149b5e441df786b5bada809bad295db7fa249b
pallino00 <lorenzo.palladino@fermi.mo.it>
2019-04-26 09:31:49 +0200

    second commit not ok

--- raccolta_dati.py
+++ raccolta_dati.py
@@ -1,7 +1,7 @@
 import pzgram
 import requests
 bot=pzgram.Bot("758461360:AAG1uPeuhZMi1k-Jt2-mknmjyZLUMztNi6I")
-bot1=pzgram.Bot("760136774:AAGaGil-U8PTbRMIQpmSyTHA-f_bF2aghPc")
+#bot1=pzgram.Bot("760136774:AAGaGil-U8PTbRMIQpmSyTHA-f_bF2aghPc")
 button1 = pzgram.create_button("Command1", data="istantanea")
 button2 = pzgram.create_button("Command1", data="pressione")
 button3 = pzgram.create_button("Command1", data="altitudine")
@@ -9,10 +9,11 @@ button4 = pzgram.create_button("Command1", data="luce")
 
 keyboard_array = [["button1", "button2","button3","button4"]]
 keyboard=pzgram.create_keyboard(keyboard_array)
-pzgram.Chat.send("press a button",reply_markup=keyboard)
+#chat.send("Select a command", reply_markup=keyboard)
 
 def sendIstantanea(chat):
 	#chat.send
+	pass
 
 def ricerca_istantanea(chat,message):
 	chat.send("ricerca istantanea avviata!")
@@ -66,5 +67,4 @@ bot.set_commands({ "istantanea": sendIstantanea})
 #bot.set_commands({"ricerca_10_minuti":ricerca_dieci_minuti,"ricerca_un_ora":ricerca_un_ora})
        #fa partire il bot
 #pzgram.Chat(bot1, 553695220).send("sm")     #manda un messaggio su telegram
-pzgram.Chat.send("Script runned ")
 bot.run()
