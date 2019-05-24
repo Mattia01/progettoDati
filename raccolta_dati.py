5eadf777227bd823b13407fb596daeee07079482
pallino00 <lorenzo.palladino@fermi.mo.it>
2019-05-24 09:04:42 +0200

    ultimato

--- raccolta_dati.py
+++ raccolta_dati.py
@@ -1,58 +1,66 @@
 import pzgram
 import requests
-bot=pzgram.Bot("758461360:AAG1uPeuhZMi1k-Jt2-mknmjyZLUMztNi6I")
+import threading
+import mosquitto
+import time
+media=0
+#bot=pzgram.Bot("758461360:AAG1uPeuhZMi1k-Jt2-mknmjyZLUMztNi6I")
 #bot1=pzgram.Bot("760136774:AAGaGil-U8PTbRMIQpmSyTHA-f_bF2aghPc")
 def setbot(chat):
-	button1 = pzgram.create_keyboard([["istantanea","1min","10min","1ora"]])
+	button1 = pzgram.create_keyboard([["/istantanea","/un_min"],["/dieci_min","/una_ora"]])
 	chat.send("Select a command", reply_markup=button1)
 
-
 def sendIstantanea(chat):
-	chat.send("ciao")
-	#pass
-def ricerca_istantanea(chat,message):
 	chat.send("ricerca istantanea avviata!")
-	chat.send(message)
-	#bot.set_next(chat,nome della funzione da eseguire)
-	#pass
+	t=threading.Thread(target=mosquitto.istantanea)
+	t.start()
+	t1 = threading.Thread(target = mosquitto.calcolo)
+	t1.start()
+	t1.join()
+	chat.send(media)
 
 def ricerca_un_minuto(chat,message):
+	global media
 	chat.send("ricerca da un minuto avviata!")
-	#pass
-
-#def ricerca_dieci_minuti(chat,message):
-#	chat.send("ricerca da 10 minuti avviata!")
-#	pass
-#def ricerca_un_ora(chat,message):
-#	chat.send("ricerca da un'ora avviata!")
-#	pass
-#553695220
-
-#def ric1():
-	#scelta=1
-	#mqtt.ricerca()
+	t = threading.Thread(target =mosquitto.ricevi_dati_minuto)
+	t.start()
+	t1 = threading.Thread(target = mosquitto.calcolo2)
+	t1.start()
+	t1.join()
+	chat.send(media)
 
 
-#def ric2():
-	#scelta=2
-	#mqtt.ricerca()
+
+def ricerca_dieci_minuti(chat,message):
+	chat.send("ricerca da 10 minuti avviata!")
+	t = threading.Thread(target =mosquitto.ricevi_dati_minuti)
+	t.start()
+	t1 = threading.Thread(target = mosquitto.calcolo3)
+	t1.start()
+	t1.join()
+	chat.send(media)
 
 
-#def ric3():
-	#scelta=3
-	#mqtt.ricerca()
 
+def ricerca_una_ora(chat,message):
+        chat.send("ricerca da un' ora  avviata!")
+	t = threading.Thread(target =mosquitto.ricevi_dati_ora)
+	t.start()
+	t1 = threading.Thread(target = mosquitto.calcolo4)
+	t1.start()
+	t1.join()
+	chat.send(media)
+def lauch():
+	bot.set_commands({"avvia": setbot})
+	bot.set_commands({"istantanea": sendIstantanea})
+	bot.set_commands({"un_min": ricerca_un_minuto})
+	bot.set_commands({"dieci_min": ricerca_dieci_minuti})
+	bot.set_commands({"una_ora": ricerca_una_ora})
+	bot.run()
 
-#def ric4():
-	#scelta=4
-	#mqtt.ricerca()
-#start_
-#bot.set_commands({"ricerca_istantanea": ricerca_istantanea})
-
-bot.set_commands({"avvia": setbot})
-bot.set_commands({"istantanea": sendIstantanea}) 
-
-bot.run()
+if __name__ == '__main__':
+	bot=pzgram.Bot("758461360:AAG1uPeuhZMi1k-Jt2-mknmjyZLUMztNi6I")
+	lauch()
 #bot.set_commands({"ricerca_10_minuti":ricerca_dieci_minuti,"ricerca_un_ora":ricerca_un_ora})
        #fa partire il bot
 #pzgram.Chat(bot1, 553695220).send("sm")     #manda un messaggio su telegram
